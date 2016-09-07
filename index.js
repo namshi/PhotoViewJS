d6b58ba09bc8899aa03a1e88baef143a9f4d3f3a
odino <alessandro.nadalin@gmail.com>
2016-09-07 12:23:35 +0400

    Removed jQuery dep
    * removed jquery and usage of its methods (.offset(), .find()) -- not sure the offset stuff is 100% reliable now
    * added some validation when `init`ing the plugin
    * changed `doubletap` to `zoom`: the idea is that if we want to make it configurable for people (ie. zoom on single tap) this way it's "easier"
    * didn't update the `build/` stuff (for now) as it makes it harder for reviewing

--- index.js
+++ index.js
@@ -2,50 +2,62 @@
 
 import Hammer from 'hammerjs';
 
-class ImageZoom {
+class PhotoView {
   constructor(options = {}) {
     this.options = options;
     return this;
   }
 
   init(selector) {
-    this.$elem = $(selector);
-    this.$image = this.$elem.find('img');
-    this._manager = new Hammer.Manager(this.$image[0]);
+    let container = document.querySelectorAll(selector)[0];
+
+    if (!container) {
+      console.warn(`You must provide a valid container for PhotoView (selector "${selector}" did not match any element)`);
+      return;
+    }
+
+    this.image = container.querySelectorAll('img')[0];
+
+    if (!this.image) {
+      console.warn(`You must have a valid img tag inside your container`);
+      return;
+    }
+
+    this._manager = new Hammer.Manager(this.image);
     this._registerGestures();
     this._registerEvents();
     this.scale = 1;
-    this.imageOffset = this.$image.offset();
+    this.imageOffset = {top: this.image.offsetTop, left: this.image.offsetLeft};
     this.deltaX = 0;
     this.deltaY = 0;
   }
 
   _registerGestures() {
-    const doubleTap = new Hammer.Tap({event: 'doubleTap', taps: 2 });
+    const zoom = new Hammer.Tap({event: 'zoom', taps: 2 });
     const pan = new Hammer.Pan({direction: Hammer.DIRECTION_ALL, threshold: 0});
     const pinch = new Hammer.Pinch();
 
     pan.recognizeWith([pinch]);
     pinch.requireFailure([pan]);
-    this._manager.add([doubleTap, pan, pinch]);
+    this._manager.add([zoom, pan, pinch]);
   }
 
   _registerEvents() {
-    this._manager.on('doubleTap', e => {
+    this._manager.on('zoom', e => {
       let {x, y} = e.center;
       this.scale = this.scale > 1 ? 1 : 3;
-      this.$image.css('transition', 'transform 0.5s');
+      this.image.style.transition = 'transform 0.5s';
       this._transform(x, y, this.scale );
     });
 
     this._manager.on('pinch', e => {
-      this.$image.css('transition', 'none');
+      this.image.style.transition = 'none';
       let {x, y} = e.center;
       this._transform(x,y,e.scale);
     });
 
     this._manager.on('panstart', e => {
-      this.$image.css('transition', 'none');
+      this.image.style.transition = 'none';
     });
 
     this._manager.on('pan', e => {
@@ -55,11 +67,8 @@ class ImageZoom {
 
       this.currentDeltaX = this.deltaX + e.deltaX;
       this.currentDeltaY = this.deltaY + e.deltaY;
-
-      this.$image.css({
-        'transition': 'none',
-        'transform':`translate3d(${this.currentDeltaX}px, ${this.currentDeltaY}px, 0px) scale(${this.scale})`
-      });
+      this.image.style.transition = 'none';
+      this.image.style.transform = `translate3d(${this.currentDeltaX}px, ${this.currentDeltaY}px, 0px) scale(${this.scale})`;
     });
 
     this._manager.on('panend', e => {
@@ -78,10 +87,8 @@ class ImageZoom {
       y-= this.imageOffset.top;
     }
 
-    this.$image.css({
-      'transform-origin': `${x}px ${y}px`,
-      'transform': `scale(${scale})`
-    });
+    this.image.style['transformOrigin'] = `${x}px ${y}px`;
+    this.image.style.transform = `scale(${scale})`;
 
     this.x = x;
     this.y = y;
@@ -91,7 +98,7 @@ class ImageZoom {
   _unregisterEvents() {
     this._manager.off('pan');
     this._manager.off('pinch');
-    this._manager.off('doubletap');
+    this._manager.off('zoom');
   }
 
   destroy() {
@@ -101,5 +108,5 @@ class ImageZoom {
 }
 
 export default {
-  ImageZoom
+  PhotoView
 };
