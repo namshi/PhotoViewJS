7904483d69ccd326b9d30257462551f315415447
Shidhin C R <shidhincr@gmail.com>
2016-09-22 18:14:01 +0530

    Refactoring to support carousel implementation

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 import Hammer from 'hammerjs';
 
-class PhotoView {
+class PhotoViewManager {
   constructor(options = {}) {
     this.options = options;
     return this;
   }
 
   init(selector) {
-    let container = document.querySelectorAll(selector)[0];
+    let container = typeof selector === 'string' ? document.querySelectorAll(selector)[0] : selector;
 
     if (!container) {
       console.warn(`You must provide a valid container for PhotoView (selector "${selector}" did not match any element)`);
@@ -27,14 +27,14 @@ class PhotoView {
     this._registerGestures();
     this._registerEvents();
     this.scale = 1;
-    this.imageOffset = {top: this.image.offsetTop, left: this.image.offsetLeft};
     this.deltaX = 0;
     this.deltaY = 0;
+    return this;
   }
 
   _registerGestures() {
-    const zoom = new Hammer.Tap({event: 'zoom', taps: 2 });
-    const pan = new Hammer.Pan({direction: Hammer.DIRECTION_ALL, threshold: 0});
+    const zoom = new Hammer.Tap({ event: 'zoom', taps: 2 });
+    const pan = new Hammer.Pan({ direction: Hammer.DIRECTION_ALL, threshold: 0 });
     const pinch = new Hammer.Pinch();
 
     pan.recognizeWith([pinch]);
@@ -47,13 +47,13 @@ class PhotoView {
       let {x, y} = e.center;
       this.scale = this.scale > 1 ? 1 : 3;
       this.image.style.transition = 'transform 0.5s';
-      this._transform(x, y, this.scale );
+      this._transform(x, y, this.scale);
     });
 
     this._manager.on('pinch', e => {
       this.image.style.transition = 'none';
       let {x, y} = e.center;
-      this._transform(x,y,e.scale);
+      this._transform(x, y, e.scale);
     });
 
     this._manager.on('panstart', e => {
@@ -61,30 +61,32 @@ class PhotoView {
     });
 
     this._manager.on('pan', e => {
-      if(this.scale === 1){
+      if (this.scale === 1) {
         return;
       }
 
-      this.currentDeltaX = this.deltaX + e.deltaX;
-      this.currentDeltaY = this.deltaY + e.deltaY;
+      e.srcEvent.stopPropagation();
+
+      this.currentDeltaX = ( isNaN(this.deltaX) ? 0 : this.deltaX ) + e.deltaX;
+      this.currentDeltaY = ( isNaN(this.deltaY) ? 0 : this.deltaY ) + e.deltaY;
       this.image.style.transition = 'none';
       this.image.style.transform = `translate3d(${this.currentDeltaX}px, ${this.currentDeltaY}px, 0px) scale(${this.scale})`;
     });
 
     this._manager.on('panend', e => {
-        this.deltaX = this.currentDeltaX;
-        this.deltaY = this.currentDeltaY;
+      this.deltaX = this.currentDeltaX;
+      this.deltaY = this.currentDeltaY;
     })
   }
 
   _transform(x, y, scale) {
-    scale = scale < 1 ? 1: scale;
+    scale = scale < 1 ? 1 : scale;
 
-    if(scale === 1){
+    if (scale === 1) {
       x = this.x; y = this.y;
     } else {
-      x-= this.imageOffset.left;
-      y-= this.imageOffset.top;
+      x -= this.image.offsetLeft;
+      y -= this.image.offsetTop;
     }
 
     this.image.style['transformOrigin'] = `${x}px ${y}px`;
@@ -107,6 +109,22 @@ class PhotoView {
   }
 }
 
-export default {
-  PhotoView
-};
+class PhotoView {
+  constructor(selector, options = {}) {
+    this.instances = [];
+    document.querySelectorAll(selector).forEach(item => {
+      this.instances.push(
+        new PhotoViewManager(options).init(item)
+      );
+    });
+  }
+
+  destroy() {
+    this.instances.forEach(photoViewInstance => {
+      photoViewInstance.destroy();
+    });
+    this.instances = null;
+  }
+}
+
+export default PhotoView;
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
         libraryTarget: 'umd'
     },
     devtool: 'source-map',
+    cache: true,
     module: {
         loaders: [{
             test: /\.js$/,
